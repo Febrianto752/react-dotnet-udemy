b151a3f8665480114e8fab6636e35d5da2364a5d
Febrianto752 <febrianto.bekasi@gmail.com>
2024-01-12 09:06:13 +0700

    feat: update activity detail page

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,9 +1,13 @@
-import { Button, Card, Image } from "semantic-ui-react";
+import { Button, Card, Grid, Image } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useParams } from "react-router-dom";
 import { useEffect } from "react";
+import ActivityDetailedHeader from "./ActivityDetailedHeader";
+import ActivityDetailedInfo from "./ActivityDetailedInfo";
+import ActivityDetailedChat from "./ActivityDetailedChat";
+import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
 
 function AcitivityDetails() {
   const { activityStore } = useStore();
@@ -23,36 +27,49 @@ function AcitivityDetails() {
   }
 
   return (
-    <Card>
-      <Image src={`/assets/images/category/film.jpg`} />
-      <Card.Content>
-        <Card.Header>{activity.title}</Card.Header>
-        <Card.Meta>
-          <span className="date">{activity.date}</span>
-          <Card.Description>{activity.description}</Card.Description>
-        </Card.Meta>
-      </Card.Content>
-
-      <Card.Content extra>
-        <Button.Group widths={2}>
-          <Button
-            as={Link}
-            to={`/manage/${activity.id}`}
-            basic
-            color="blue"
-            content="Edit"
-          />
-          <Button
-            as={Link}
-            to="/activities"
-            basic
-            color="grey"
-            content="Cancel"
-          />
-        </Button.Group>
-      </Card.Content>
-    </Card>
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityDetailedHeader activity={activity} />
+        <ActivityDetailedInfo activity={activity} />
+        <ActivityDetailedChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetailedSidebar />
+      </Grid.Column>
+    </Grid>
   );
+
+  // return (
+  //   <Card>
+  //     <Image src={`/assets/images/category/film.jpg`} />
+  //     <Card.Content>
+  //       <Card.Header>{activity.title}</Card.Header>
+  //       <Card.Meta>
+  //         <span className="date">{activity.date}</span>
+  //         <Card.Description>{activity.description}</Card.Description>
+  //       </Card.Meta>
+  //     </Card.Content>
+
+  //     <Card.Content extra>
+  //       <Button.Group widths={2}>
+  //         <Button
+  //           as={Link}
+  //           to={`/manage/${activity.id}`}
+  //           basic
+  //           color="blue"
+  //           content="Edit"
+  //         />
+  //         <Button
+  //           as={Link}
+  //           to="/activities"
+  //           basic
+  //           color="grey"
+  //           content="Cancel"
+  //         />
+  //       </Button.Group>
+  //     </Card.Content>
+  //   </Card>
+  // );
 }
 
 export default observer(AcitivityDetails);
